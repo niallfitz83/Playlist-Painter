f70ec55a9acdca33e5b5cfb3d0678e190b5dc3d6
niallfitz83 <118831069+niallfitz83@users.noreply.github.com>
2023-11-19 14:59:58 +0000

    Add files via upload

--- getPlaylist.js
+++ getPlaylist.js
@@ -1,45 +1,31 @@
-// getPlaylists.js
-
 const express = require('express');
-const fetch = require('node-fetch'); // make sure you've installed node-fetch
 const router = express.Router();
+const fetch = require('node-fetch');
 
-// Helper function to get playlists from Spotify API
-const getSpotifyPlaylists = async (accessToken) => {
-    const url = 'https://api.spotify.com/v1/me/playlists';
-    const headers = {
-        'Authorization': `Bearer ${accessToken}`
-    };
 
-    try {
-        const response = await fetch(url, { headers: headers });
-        const data = await response.json();
-        if (!response.ok) {
-            // Handle any errors from the Spotify API
-            throw new Error(data.error.message);
-        }
-        return data.items; // Assuming you want to return just the array of playlist items
-    } catch (error) {
-        // Handle errors such as network issues or JSON parsing problems
-        console.error('Error fetching playlists:', error);
-        throw error;
-    }
-};
 
-// Route to get the playlists
-router.get('/', async (req, res) => {
-    try {
-        // Here you need to have a way to get the access token, for example from the session
-        const accessToken = req.session.user.access_token;
-        if (!accessToken) {
-            return res.status(401).send('Access Token is missing or expired');
-        }
-        
-        const playlists = await getSpotifyPlaylists(accessToken);
-        res.json(playlists); // Send the playlists to the client
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
+app.get('/playlists', async function (req, res) {
+  if (!req.session.access_token) {
+      return res.status(401).send('Access token is missing');
+  }
+
+  const playlistsEndpoint = 'https://api.spotify.com/v1/me/playlists';
+  const authOptions = {
+      headers: { 'Authorization': 'Bearer ' + req.session.access_token }
+  };
+
+  try {
+      const response = await fetch(playlistsEndpoint, authOptions);
+      if (response.ok) {
+          const data = await response.json();
+          res.send(data); // Send playlist data to the client
+      } else {
+          res.status(response.status).send('Failed to fetch playlists');
+      }
+  } catch (error) {
+      console.error('Error fetching playlists:', error);
+      res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
